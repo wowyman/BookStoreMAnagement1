725d277699ea589a0848bf849ac4b801de5a4563
Cuong <Cuong232k1@gmail.com>
2021-05-04 23:26:01 +0700

    04*05*2021

--- src/sample/controller/Managementcardguest.java
+++ src/sample/controller/Managementcardguest.java
@@ -4,15 +4,21 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import sample.model.Card;
 import sample.model.Customer;
 import sample.model.CustomerDB;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Managementcardguest {
@@ -27,8 +33,79 @@ public class Managementcardguest {
     @FXML
     Button them,sua,xoa,luu,chapnhan,huy;
     String mt;
-    public void handle() throws SQLException, ClassNotFoundException {
-        mt = mathe.getText();
+    public void them_But(ActionEvent event) throws SQLException, ClassNotFoundException {
+        try {
+            AnchorPane root1 = FXMLLoader.load(getClass().getResource("/src/sample/view/Menu.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.setTitle("Menu");
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void xoa_but(ActionEvent event) {
+        try {
+            AnchorPane root1 = FXMLLoader.load(getClass().getResource("/src/sample/view/Menu.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.setTitle("Menu");
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sua_but(ActionEvent event) {
+        try {
+            AnchorPane root1 = FXMLLoader.load(getClass().getResource("/src/sample/view/Menu.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.setTitle("Menu");
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void luu_but(ActionEvent event) {
+        try {
+            AnchorPane root1 = FXMLLoader.load(getClass().getResource("/src/sample/view/Menu.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.setTitle("Menu");
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void chapnhan_but(ActionEvent event) {
+        try {
+            AnchorPane root1 = FXMLLoader.load(getClass().getResource("/src/sample/view/Menu.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.setTitle("Menu");
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void huy_but(ActionEvent event) {
+        try {
+            AnchorPane root1 = FXMLLoader.load(getClass().getResource("/src/sample/view/Menu.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.setTitle("Menu");
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void handle_makhach() throws SQLException, ClassNotFoundException {
         ObservableList<Customer> customers = CustomerDB.search_all_customer();
         ArrayList<String> list_cusmk = new ArrayList<>();
         for (Customer customer:customers){
@@ -37,25 +114,29 @@ public class Managementcardguest {
         ObservableList<String>listmk = FXCollections.observableArrayList(list_cusmk);
         System.out.println(list_cusmk.size());
         makhach.setItems(listmk);
+        System.out.println(CustomerDB.search_customer_by_CMND(makhach.getValue()).size());
+    }
+
+    public void handle_loaithe() {
         ObservableList<String> list_loaithe = FXCollections.observableArrayList("the muon","the doc");
         loaithe.setItems(list_loaithe);
     }
 
-    public void them_But(ActionEvent event) {
+    public void handle_ngaycap() throws SQLException, ClassNotFoundException {
+        if (loaithe.getValue().equals("the doc")){
+            String ngayCapThe = CustomerDB.search_customer_by_CMND(makhach.getValue()).get(0).getThedoc().getNgayCapThe();
+            String mathe_find = CustomerDB.search_customer_by_CMND(makhach.getValue()).get(0).getThedoc().getCardNumber();
+            mathe.setText(mathe_find);
+        }
+        else {
+            String ngayCapThe = CustomerDB.search_customer_by_CMND(makhach.getValue()).get(0).getThemuon().getNgayCapThe();
+            String mathe_find = CustomerDB.search_customer_by_CMND(makhach.getValue()).get(0).getThemuon().getCardNumber();
+            mathe.setText(mathe_find);
+        }
+        LocalDate localDate =LocalDate.of(2021,05,04);
+        ngaycap.setValue(localDate);
     }
-
-    public void xoa_but(ActionEvent event) {
-    }
-
-    public void sua_but(ActionEvent event) {
-    }
-
-    public void luu_but(ActionEvent event) {
-    }
-
-    public void chapnhan_but(ActionEvent event) {
-    }
-
-    public void huy_but(ActionEvent event) {
+    public void handle() throws SQLException, ClassNotFoundException {
+        mt = mathe.getText();
     }
 }
